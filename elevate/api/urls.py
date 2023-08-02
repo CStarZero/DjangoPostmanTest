33e57be0154c9b27e450d2ff96d1053acbf4a813
CStarZero <achernar.uk@gmail.com>
2023-08-02 14:15:59 -0700

    Update to Django API Postman Test

---
+++ elevate/api/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+
+    path('products', views.product_list, name='products'),
+
+    path('product/<int:pk>', views.product, name='product'),
+
+]
